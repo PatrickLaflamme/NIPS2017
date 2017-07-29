f394529479a6da0eede6678cb3b94c71e7f86883
PatrickLaflamme <Patrickj.laflamme@gmail.com>
2017-07-29 10:37:14 -0700

    bug fixes

--- model/attempt1.py
+++ model/attempt1.py
@@ -43,7 +43,7 @@ def gen_mlp_weights(num_input, num_output, n_hidden, n_layers, name=''):
 
     for layer_number in range(n_layers+1):
 
-        weights[str(layer_number)+'_'+name] = tf.Variable(tf.random_normal([n_hidden[layer_number], n_hidden[layer_number+1]],stddev=1/(n_hidden[layer_number+1]), name = name + str(layer_number))
+        weights[str(layer_number)+'_'+name] = tf.Variable(tf.random_normal([n_hidden[layer_number], n_hidden[layer_number+1]],stddev=1/(n_hidden[layer_number+1])), name = name + str(layer_number))
 
         biases[str(layer_number)+'_'+name] = tf.Variable(tf.random_normal([n_hidden[layer_number+1]],stddev=1/(n_hidden[layer_number+1])), name = name + str(layer_number) + '_bias')
 
