9ccb700ad845ab05b85d7d73840dde57f447924d
PatrickLaflamme <Patrickj.laflamme@gmail.com>
2017-07-30 20:15:02 -0700

    debugging

--- model/attempt2.py
+++ model/attempt2.py
@@ -295,9 +295,7 @@ class ActorCriticDDPG(object):
         critic_loss, actor_loss, _ = self.session.run([self.critic_loss, self.actor_loss, self.train_op], feed_dict = {self.states: old_obs,
                                      self.reward: reward,
                                      self.taken_actions: action,
-                                     self.
-
-        })
+                                     self.result_state: obs})
 
         self.tot_critic_loss += critic_loss
         self.tot_actor_loss += actor_loss
