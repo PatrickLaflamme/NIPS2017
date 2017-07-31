41c0bef4c5bdcee577c9225b15629b03dbff5d80
PatrickLaflamme <Patrickj.laflamme@gmail.com>
2017-07-30 20:50:00 -0700

    debugging

--- model/attempt2.py
+++ model/attempt2.py
@@ -294,7 +294,7 @@ class ActorCriticDDPG(object):
 
         old_obs, reward, action, obs = [sample[0] for sample in sample_states], [sample[1] for sample in sample_states], [sample[2] for sample in sample_states], [sample[3] for sample in sample_states]
 
-        critic_loss, actor_loss, _ = self.session.run([self.critic_loss, self.actor_loss, self.train_op], feed_dict = {self.states: [old_obs],
+        critic_loss, actor_loss, _ = self.session.run([self.critic_loss, self.actor_loss, self.train_op], feed_dict = {self.states: old_obs,
                                      self.reward: [reward],
                                      self.taken_actions: action,
                                      self.result_state: obs})
