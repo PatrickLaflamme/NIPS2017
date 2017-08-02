f283db2fb29b5ed31afae17c01912211ed9b6ad3
PatrickLaflamme <Patrickj.laflamme@gmail.com>
2017-08-02 13:12:58 -0700

    bug fixes

--- model/attempt2.py
+++ model/attempt2.py
@@ -338,9 +338,9 @@ class ActorCriticDDPG(object):
 
         [actions] = self.session.run([self.chosen_actions], feed_dict={self.states:[observation]})
 
-        print(actions[0])
+        print(actions[0,:].tolist())
 
-        return actions[0]
+        return actions[0,:].tolist()
 
 
 
