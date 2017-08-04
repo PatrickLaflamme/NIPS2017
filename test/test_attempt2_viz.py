c10787efb977db7967c8c35948090f7f79a2969d
PatrickLaflamme <Patrickj.laflamme@gmail.com>
2017-08-04 15:57:51 -0700

    allow for viz

---
+++ test/test_attempt2_viz.py
@@ -0,0 +1,38 @@
+import numpy as np
+import tensorflow as tf
+import opensim as osim
+import sys
+from osim.env import RunEnv
+
+sys.path.append('../model')
+
+import attempt2
+
+# Settings
+remote_base = "http://grader.crowdai.org:1729"
+crowdai_token = sys.argv[1]
+
+client = = RunEnv(visualize=True)
+# Create environment
+observation = client.reset(difficulty=2)
+
+with tf.Session() as session:
+
+    model = attempt2.ActorCriticDDPG(session,
+                                    tf.train.GradientDescentOptimizer(learning_rate=0.01),
+                                    41,
+                                    18,
+                                    tf.train.Saver)
+
+    model.restore("../model_attempt2_train_1/")
+
+    for i in range(1500):
+
+        [observation, reward, done, info] = client.step(model.gen_actions(observation))
+
+        print(reward)
+
+        if done:
+            observation = client.reset(difficulty=2)
+            if not observation:
+                break
